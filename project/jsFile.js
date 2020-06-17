c8899b819d30fc4e71ceed30a800348331b6c307
Jenya Habinski <tabey123@mail.ru>
2020-06-17 16:36:57 +0300

    project

--- project/jsFile.js
+++ project/jsFile.js
@@ -1,1 +1,15 @@
-console.log(window.innerWidth)
+let basket=document.querySelector('.basket');
+let modDiv=document.querySelector('.modal');
+let allDiv=document.querySelector('.allItems');
+let cross=document.querySelector('.cross');
+
+basket.addEventListener('click', modalOpen);
+cross.addEventListener('click', crossOn)
+
+function modalOpen(){
+    modDiv.style.display='block';
+}
+
+function crossOn(){
+    modDiv.style.display='none';
+}
